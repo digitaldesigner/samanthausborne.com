7078b14fb7ec80477f8400d0d48f7e2bcfb4fb6c
Buzz Usborne <hello@buzzusborne.com>
2017-10-19 11:45:51 +1100

    Grid view and scroll-to images

--- thekey/_assets/js/sambo.js
+++ thekey/_assets/js/sambo.js
@@ -9,6 +9,25 @@ $(document).ready(function(){
 		$('.fullImage').toggleClass('grid');
 		$(this).toggleClass('vert');
 		$('.fullImage img').removeClass('first');
+		
+		$('html, body').animate({
+			scrollTop: ($('.fullImage').offset().top) - 30
+		}, 300);
+	});
+	
+	$('img').click(function(){
+		
+		if($('.fullImage').hasClass('grid')){
+			$('.fullImage').removeClass('grid');
+			$('#grid').removeClass('vert');
+			
+			$('html, body').animate({
+				scrollTop: ($(this).offset().top) - 5
+			}, 300);
+		}
+		
+		
+		
 	});
 	
 });
